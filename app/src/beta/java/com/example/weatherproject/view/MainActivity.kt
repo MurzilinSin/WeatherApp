9f156689fb787235ee10c6b9c1ede444538d9d62
MurzilinSin <asair4ik@mail.ru>
2021-06-26 20:45:49 +0300

    this_is_the_end

---
+++ app/src/beta/java/com/example/weatherproject/view/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.weatherproject.view
+
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.weatherproject.R
+import com.example.weatherproject.databinding.MainActivityBinding
+import com.example.weatherproject.view.contentProvider.ContentProviderFragment
+import com.example.weatherproject.view.history.HistoryFragment
+import com.example.weatherproject.view.main.MainBroadcast
+import com.example.weatherproject.view.main.MainFragment
+import com.example.weatherproject.googlemaps.GoogleMapsFragment
+
+private val receiver = MainBroadcast()
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        registerReceiver(receiver, IntentFilter(CONNECTIVITY_SERVICE))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_history, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_history -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .replace(R.id.container, HistoryFragment.newInstance())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            R.id.menu_content_provider -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                            .replace(R.id.container,  ContentProviderFragment.newInstance())
+                            .addToBackStack("")
+                            .commitAllowingStateLoss()
+                }
+                true
+            }
+            R.id.menu_google_map -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .replace(R.id.container, GoogleMapsFragment())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
+    }
+}
